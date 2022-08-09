f7310cd4ac376a00ef68922488d022f2a9e721de
dagim360 <68782461+dagim360@users.noreply.github.com>
2022-08-09 15:46:50 -0700

    c

--- OneDrive/Desktop/dagitube/myvideoapp/src/App.js
+++ OneDrive/Desktop/dagitube/myvideoapp/src/App.js
@@ -1,32 +1,49 @@
+
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
 import styled from "styled-components";
-import  Menu   from "./components/Menu";
-import  Navbar  from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Video from "./components/pages/Video";
+
 
 const Container = styled.div`
-  display:flex;
- 
-
+  display: flex;
 `;
-
 
 const Main = styled.div`
-
-  flex:7;
-
+  flex: 7;
+ 
 `;
-const Wrapper = styled.div``;
-function App() {
-  return (
-    <Container>
-      <Menu />
-      <Main>
-       <Navbar />
-       <Wrapper>
-        video cards
-      </Wrapper>
+const Wrapper = styled.div`
+  padding: 22px 96px;
+`;
 
-      </Main>
-    </Container>
+function App() {
+  
+
+  return (
+  
+      <Container>
+        <BrowserRouter>
+          <Menu/>
+          <Main>
+            <Navbar />
+            <Wrapper>
+              <Routes>
+                <Route path="/">
+                  <Route index element={<Home />} />
+                  
+                  <Route path="video">
+                    <Route path=":id" element={<Video />} />
+                  </Route>
+                </Route>
+              </Routes>
+            </Wrapper>
+          </Main>
+        </BrowserRouter>
+      </Container>
+   
   );
 }
 
